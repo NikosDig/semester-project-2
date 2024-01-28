b4d246cc4af104573348c39aac3077c4d93c4d6c
NikosDigalakis <nikos_23_chania@hotmail.com>
2024-01-28 16:32:16 +0100

    dont show profile if user is not loged in

--- javascript/pagesMainFile/profile.mjs
+++ javascript/pagesMainFile/profile.mjs
@@ -1,10 +1,28 @@
 import { loadJWT } from "./../storageJWT/loadJWT.mjs";
 import { fixProfile } from "../populateProfile/fixProfileValues.mjs";
 import { logOUt } from "../storageJWT/logOut.mjs";
+import { checkUserLoggedIn } from "./../isLogedIn/checkUserLoggedIn.mjs";
 
 // Use an async function to load the user data
 const loadUser = async () => {
   try {
+    // Check if the user is logged in
+    const isLoggedIn = checkUserLoggedIn();
+
+    const userNotLoggedInElement = document.querySelector(".userNotLogedIn");
+    const userLoggedInElement = document.querySelector(".userLogedIn");
+
+    if (!isLoggedIn) {
+      // If not logged in, handle UI accordingly
+      if (userNotLoggedInElement && userLoggedInElement) {
+        userNotLoggedInElement.classList.remove("d-none");
+        userLoggedInElement.classList.add("d-none");
+      }
+
+      return null;
+    }
+
+    // If logged in, proceed to load user data
     const loadedUser = await loadJWT("user");
     return loadedUser;
   } catch (error) {
@@ -15,10 +33,11 @@ const loadUser = async () => {
 
 // Call the async function to load the user data
 loadUser().then((user) => {
-  console.log(user);
-
-  // Call the fixProfile function after the user data is loaded
-  fixProfile(user);
+  if (user) {
+    // Call the fixProfile function after the user data is loaded
+    fixProfile(user);
+    //console.log(user)
+  }
 });
 
 logOUt();
